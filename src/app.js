5f730c75bd9a4b39dce39b5e3ce6b7e14884bf86
Lidia GDC <lidiagdcprogramacion@gmail.com>
2021-05-30 19:30:15 +0200

    Added real forecast data by API call

--- src/app.js
+++ src/app.js
@@ -21,37 +21,52 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function showForecast(response) {
-  console.log(response.data.daily);
-  let forecastElement = document.querySelector("#forecast");
+function formatWeekDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
 
   let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
-    "Monday",
   ];
+
+  return days[day];
+}
+function showForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2"> 
-      <div class ="forecast-date"> ${day}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2"> 
+      <div class ="forecast-date"> ${formatWeekDay(forecastDay.dt)}</div>
       <img class="forecast-icon"
-       src="http://openweathermap.org/img/wn/11n@2x.png"
+       src="http://openweathermap.org/img/wn/${
+         forecastDay.weather[0].icon
+       }@2x.png"
        alt=""
        width="50px"
        />
        <div class ="weather-forecast-temperatures">
-         <span class="forecast-temperature-max"> 20°C </span>
-         <span class="forecast-temperature-min"> 10°C </span>
+         <span class="forecast-temperature-max"> ${Math.round(
+           forecastDay.temp.max
+         )}°C </span>
+         <span class="forecast-temperature-min"> ${Math.round(
+           forecastDay.temp.min
+         )}°C </span>
       </div>
       </div>  
    `;
+    }
   });
   forecastHTML = forecastHTML + `</div> `;
   forecastElement.innerHTML = forecastHTML;
